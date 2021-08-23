fcb85342611f2da6db77226f2b0efb669dbb43c3
Francisco Valle <vallecesco@gmail.com>
2021-08-23 11:56:13 -0600

    Probando con expo

--- Guia 3-Configuración de Entorno de Trabajo con React Native/cars/index.js
+++ Guia 3-Configuración de Entorno de Trabajo con React Native/cars/index.js
@@ -2,6 +2,9 @@ import React,{Component} from 'react';
 import {AppRegistry} from 'react-native'
 import {Text,View} from 'react-native';
 
+// npx react-native run-android
+// npx @react-native-community/cli doctor
+
 const textosaludo=()=>{
     return(
         <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
