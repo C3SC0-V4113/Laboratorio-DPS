1bd7fb69c4dd2ce2cc549aa5c79095926e73ee93
Francisco Valle <vallecesco@gmail.com>
2021-08-23 19:23:39 -0600

    Modificando por titulos con autos e imagenes agregadas

--- Guia 3-Configuración de Entorno de Trabajo con React Native/cars/index.js
+++ Guia 3-Configuración de Entorno de Trabajo con React Native/cars/index.js
@@ -1,16 +1,61 @@
 import React,{Component} from 'react';
 import {AppRegistry} from 'react-native'
 import {Text,View} from 'react-native';
+import { SafeAreaView,FlatList,StyleSheet,StatusBar, Image } from 'react-native';
 
 // npx react-native run-android
 // npx @react-native-community/cli doctor
 
-const textosaludo=()=>{
+const DATA=[
+    {
+        id:'1',
+        title:'Toyota',
+    },
+    {
+        id:'2',
+        title:'Mazda',
+    },
+    {
+        id:'3',
+        title:'Mitsubishi',
+    },
+];
+
+const Item=({title})=>(
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+);
+
+const App=()=>{
+    const renderItem=({item})=>(
+        <Item title={item.title}/>
+    );
     return(
-        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-            <Text>"Hola Mundo, soy CESCO"</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item=>item.id}
+            />
+        </SafeAreaView>
     );
 }
 
-AppRegistry.registerComponent("main",()=>textosaludo);
+const styles=StyleSheet.create({
+    container:{
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item:{
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title:{
+        fontSize: 32,
+    },
+});
+
+AppRegistry.registerComponent("main",()=>App);
