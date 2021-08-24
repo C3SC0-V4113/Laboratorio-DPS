be7610949dc67eab30bfba89e879fbf2179d601e
Francisco Valle <vallecesco@gmail.com>
2021-08-23 22:53:56 -0600

    Funciona con todo y fotos

--- Guia 3-Configuración de Entorno de Trabajo con React Native/cars/index.js
+++ Guia 3-Configuración de Entorno de Trabajo con React Native/cars/index.js
@@ -5,31 +5,37 @@ import { SafeAreaView,FlatList,StyleSheet,StatusBar, Image } from 'react-native'
 
 // npx react-native run-android
 // npx @react-native-community/cli doctor
+//expo start --clear
+//keytool -genkey -v -keystore my-upload-key.keystore -alias my-key-alias -keyalg RSA -keysize 2048 -validity 10000
 
 const DATA=[
     {
         id:'1',
         title:'Toyota',
+        src:require('./src/imgs/toyota.jpg'),
     },
     {
         id:'2',
         title:'Mazda',
+        src:require('./src/imgs/mazda.jpg'),
     },
     {
         id:'3',
         title:'Mitsubishi',
+        src:require('./src/imgs/mitsubishi.jpeg'),
     },
 ];
 
-const Item=({title})=>(
+const Item=({title,img})=>(
     <View style={styles.item}>
         <Text style={styles.title}>{title}</Text>
+        <Image style={styles.img} source={img}/>
     </View>
 );
 
 const App=()=>{
     const renderItem=({item})=>(
-        <Item title={item.title}/>
+        <Item title={item.title} img={item.src}/>
     );
     return(
         <SafeAreaView style={styles.container}>
@@ -52,9 +58,18 @@ const styles=StyleSheet.create({
         padding: 20,
         marginVertical: 8,
         marginHorizontal: 16,
+        alignItems:'center',
     },
     title:{
         fontSize: 32,
+    },
+    img:{
+        width: 200,
+        height:125,
+        borderWidth:2,
+        borderColor:'#d35647',
+        resizeMode:'contain',
+        margin:8,
     },
 });
 
