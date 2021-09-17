074cdb35233a6544c557e693eda41f58e4a01ff0
Francisco Valle <vallecesco@gmail.com>
2021-09-16 23:41:02 -0600

    Pasos de la guia completados

--- Guia 6-Layout and Flexbox/agenciaviajes/App.js
+++ Guia 6-Layout and Flexbox/agenciaviajes/App.js
@@ -1,71 +1,185 @@
-import React from 'react';
-import {View, StyleSheet,Image,Text,ScrollView} from 'react-native';
-const App = () =>{
- return(
- <>
-<View >
- <View style={{flexDirection:'row'}} >
- <Image
- style={styles.banner}
- source={require('./src/img/bg.jpg')} />
- </View>
-</View>
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  ScrollView,
+  Modal,
+  Button,
+  TouchableHighlight,
+} from 'react-native';
+const App = () => {
+  const [modalVisibleplaya, setModalVisibleplaya] = useState(false);
+  return (
+    <>
+      <ScrollView>
+        <Modal
+          transparent={true}
+          animationType="slide"
+          visible={modalVisibleplaya}
+          onRequestClose={() => {
+            alert('Modal ha sido cerrado.');
+          }}>
+          <View style={styles.vistaModal}>
+            <View style={styles.Modal}>
+              <Text style={styles.subtitulo}>Ir a la playa</Text>
+              <Text>
+                El Salvador cuenta con hermosas playas a niver Centroaméricano
+              </Text>
+              <Button
+                title="Cerrar"
+                onPress={() => {
+                  setModalVisibleplaya(!modalVisibleplaya);
+                }}></Button>
+            </View>
+          </View>
+        </Modal>
 
-<View style={styles.contenedor}>
-  <Text style={styles.titulo}>Que hacer en El Salvador</Text>
-  <ScrollView horizontal>
-    <View>
-      <Image
-        style={styles.ciudad}
-        source={require('./src/img/actividad-1.jpg')}
-      />
-    </View>
-    <View>
-      <Image
-        style={styles.ciudad}
-        source={require('./src/img/actividad-2.jpg')}
-      />
-    </View>
-    <View>
-      <Image
-        style={styles.ciudad}
-        source={require('./src/img/actividad-3.jpg')}
-      />
-    </View>
-    <View>
-      <Image
-        style={styles.ciudad}
-        source={require('./src/img/actividad-4.jpg')}
-      />
-    </View>
-    <View>
-      <Image
-        style={styles.ciudad}
-        source={require('./src/img/actividad-5.jpg')}
-      />
-    </View>
-  </ScrollView>
-</View>
-</>
- );
+        <View style={{flexDirection: 'row'}}>
+          <Image style={styles.banner} source={require('./src/img/bg.jpg')} />
+        </View>
+
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Que hacer en El Salvador</Text>
+          <ScrollView horizontal>
+            <View>
+              <TouchableHighlight
+                onPress={() => {
+                  setModalVisibleplaya(!modalVisibleplaya);
+                }}>
+                <Image
+                  style={styles.ciudad}
+                  source={require('./src/img/actividad-1.jpg')}
+                />
+              </TouchableHighlight>
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./src/img/actividad-2.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./src/img/actividad-3.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./src/img/actividad-4.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./src/img/actividad-5.jpg')}
+              />
+            </View>
+          </ScrollView>
+        </View>
+
+        <Text style={styles.titulo}>Platillos Salvadoreños</Text>
+        <View>
+          <View>
+            <Image
+              style={styles.mejores}
+              source={require('./src/img/mejores-1.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.mejores}
+              source={require('./src/img/mejores-2.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.mejores}
+              source={require('./src/img/mejores-3.jpg')}
+            />
+          </View>
+        </View>
+
+        <Text style={styles.titulo}>Rutas Turisticas</Text>
+        <View style={styles.listado}>
+          <View style={styles.listadoItem}>
+            <Image
+              style={styles.mejores}
+              source={require('./src/img/ruta-1.jpg')}
+            />
+          </View>
+          <View style={styles.listadoItem}>
+            <Image
+              style={styles.mejores}
+              source={require('./src/img/ruta-2.jpg')}
+            />
+          </View>
+          <View style={styles.listadoItem}>
+            <Image
+              style={styles.mejores}
+              source={require('./src/img/ruta-3.jpg')}
+            />
+          </View>
+          <View style={styles.listadoItem}>
+            <Image
+              style={styles.mejores}
+              source={require('./src/img/ruta-4.jpg')}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </>
+  );
 };
 const styles = StyleSheet.create({
-banner:{
- height:250,
- flex:1
-},
-titulo:{
-  fontWeight: 'bold',
-  fontSize:24,
-  marginVertical:10
-},
-contenedor:{
-  marginHorizontal:10,
-},
-ciudad:{
-  width:250,
-  height:300,
-  marginRight:10
-}
+  banner: {
+    height: 250,
+    flex: 1,
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 10,
+  },
+  contenedor: {
+    marginHorizontal: 10,
+  },
+  ciudad: {
+    width: 250,
+    height: 300,
+    marginRight: 10,
+  },
+  mejores: {
+    width: '100%',
+    height: 200,
+    marginVertical: 5,
+  },
+  listadoItem: {
+    flexBasis: '49%',
+  },
+  listado: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  vistaModal: {
+    backgroundColor: '#000000aa',
+    flex: 1,
+  },
+  Modal: {
+    backgroundColor: '#fff',
+    margin: 50,
+    padding: 40,
+    borderRadius: 10,
+    flex: 1,
+  },
+  subtitulo: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    justifyContent: 'center',
+  },
 });
 export default App;
