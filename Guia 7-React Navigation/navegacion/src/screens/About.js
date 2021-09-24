c7a091a2eb12b30023be32290d1462b762170da9
Francisco Valle <frankjose00@gmail.com>
2021-09-24 07:58:54 -0600

    ÂAgregadas las pantañllas y las dependencias

---
+++ Guia 7-React Navigation/navegacion/src/screens/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+export default function About() {
+  return (
+    <View>
+      <Text> Estamos en About</Text>
+    </View>
+  );
+}
---
+++ Guia 7-React Navigation/navegacion/src/screens/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export default function Home() {
+    return (
+        <View>
+            <Text>Estamos en Home</Text>
+        </View>
+    );
+}
