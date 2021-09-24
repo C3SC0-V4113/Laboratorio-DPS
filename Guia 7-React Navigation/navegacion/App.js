8816b9190e5e16d1ab3aca0e1c48519399f382ba
Francisco Valle <frankjose00@gmail.com>
2021-09-24 08:34:02 -0600

    Modificando un poco agregando otra dependencia

--- Guia 7-React Navigation/navegacion/App.js
+++ Guia 7-React Navigation/navegacion/App.js
@@ -14,7 +14,7 @@
 import * as React from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import Navigation from './src/navigation/Navigation';
+import Navigation from './src/Navigation/Navigation'
 export default function App() {
   return (
     <NavigationContainer>
--- Guia 7-React Navigation/navegacion/src/screens/Home.js
+++ Guia 7-React Navigation/navegacion/src/screens/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-export default function Home() {
+import { View, Text,Button } from 'react-native';
+export default function Home(props) {
+    const{navigation}=props;
     return (
         <View>
             <Text>Estamos en Home</Text>
+            <Button 
+            title='Ir a About'
+            onPress={()=>navigation.navigate('about')}
+            ></Button>
         </View>
     );
 }
