776367ab941b0dff3d878d7697432ad1fae41fbf
Francisco Valle <frankjose00@gmail.com>
2021-09-24 08:46:26 -0600

    Guia 7 avanzada

---
+++ Guia 7-React Navigation/navegacion/src/Navigation/HomeStack.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from '../screens/Home';
+const Stack = createStackNavigator();
+export default function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="home" component={Home} options={{title: 'Home'}} />
+    </Stack.Navigator>
+  );
+}
--- Guia 7-React Navigation/navegacion/src/Navigation/Navigation.js
+++ Guia 7-React Navigation/navegacion/src/Navigation/Navigation.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import Home from '../screens/Home';
-import About from '../screens/About';
-import Contact from '../screens/Contact';
-const Stack = createStackNavigator();
+import HomeStack from './HomeStack';
+import AboutStack from './AboutStack';
+import ContactStack from './ContactStack';
+
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="home" component={Home} options={{title: 'Home'}} />
-      <Stack.Screen name="about" component={About} options={{title: 'About'}} />
-      <Stack.Screen
+    <Tab.Navigator>
+      <Tab.Screen name="home" component={HomeStack} options={{title: 'Home'}} />
+      <Tab.Screen
         name="contact"
-        component={Contact}
+        component={ContactStack}
         options={{title: 'Contact'}}
       />
-    </Stack.Navigator>
+      <Tab.Screen
+        name="about"
+        component={AboutStack}
+        options={{title: 'About'}}
+      />
+    </Tab.Navigator>
   );
 }
