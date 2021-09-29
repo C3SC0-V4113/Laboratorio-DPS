8c3e550fb735995f31f02faaa59dcb557e7dee00
Francisco Valle <vallecesco@gmail.com>
2021-09-28 23:18:08 -0600

    Funcional

--- Guia 7-React Navigation/pets/src/screens/Gatos.js
+++ Guia 7-React Navigation/pets/src/screens/Gatos.js
@@ -1,27 +1,44 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import Lista from '../Components/lista';
 
-const gatos=[
-    {
-      name: 'Amy Farmau',
-      avatar_url:
-        'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-      subtitle: 'Vice Cat',
-    },
-    {
-      name: 'Chris Miauckson',
-      avatar_url:
-        'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-      subtitle: 'Vice Chairman',
-    },
-  ];
+const gatos = [
+  {
+    name: 'Siamés',
+    avatar_url:
+      'https://www.zooplus.es/magazine/wp-content/uploads/2019/01/siamkatze-1-768x512-1-768x512.jpeg',
+    subtitle: 'Tailandia',
+  },
+  {
+    name: 'Snowshoe',
+    avatar_url:
+      'https://cdn.wamiz.fr/cdn-cgi/image/quality=80,width=1200,height=675,fit=cover/https://cdn.wamiz.fr/animal/breed/cat/adult/5cd04b89c3cb4174310520.jpg',
+    subtitle: 'Estados Unidos',
+  },
+  {
+    name: 'Angora turco',
+    avatar_url:
+      'https://vidanatural.net/wp-content/uploads/2019/01/angora-turco-blanco.jpg',
+    subtitle: 'Turquía',
+  },
+  {
+    name: 'Persa',
+    avatar_url:
+      'https://sumedico.blob.core.windows.net/images/2021/01/19/curiosidadesgatopersa.jpg',
+    subtitle: 'Irán',
+  },
+  {
+    name: 'Sphynx',
+    avatar_url:
+      'https://noticiasrealessv.com/wp-content/uploads/2020/04/Gato-Sphynx-o-gato-esfinge-1.jpg',
+    subtitle: 'Canadá',
+  },
+];
 
 export default function Gato() {
   return (
     <View>
-      <Text>Estamos en Gato!</Text>
-      <Lista list={gatos}/>
+      <Lista list={gatos} />
     </View>
   );
 }
