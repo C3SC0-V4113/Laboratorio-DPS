0a28d457c76249caefed790e0e283c439ea398ae
Francisco Valle <vallecesco@gmail.com>
2021-09-29 00:45:48 -0600

    Pequeña prueba con el modal

--- Guia 7-React Navigation/pets/src/Components/lista.js
+++ Guia 7-React Navigation/pets/src/Components/lista.js
@@ -1,21 +1,102 @@
 import React, {useState} from 'react';
-import {View, Text, Image} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  Alert,
+  Modal,
+  StyleSheet,
+  Pressable,
+} from 'react-native';
 import {Card, ListItem, Button, Icon, Avatar} from 'react-native-elements';
 
-const Lista=({list})=> {
+
+
+const Lista = ({list}) => {
+    const [modalVisible, setModalVisible] = useState(false);
   return (
-    <Card containerStyle={{padding: 0}}>
-      {list.map((l, i) => (
-        <ListItem key={i} bottomDivider>
-          <Avatar source={{uri: l.avatar_url}} />
-          <ListItem.Content>
-            <ListItem.Title>{l.name}</ListItem.Title>
-            <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-      ))}
-    </Card>
+    <>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+      <Card containerStyle={{padding: 0}}>
+        {list.map((l, i) => (
+          <ListItem key={i} bottomDivider>
+            <Avatar source={{uri: l.avatar_url}} />
+            <ListItem.Content>
+              <ListItem.Title>{l.name}</ListItem.Title>
+              <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        ))}
+      </Card>
+      </Pressable>
+    </>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+});
 
 export default Lista;
