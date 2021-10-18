0f314076b1723663a180283c863594205fb8712a
Francisco Valle <vallecesco@gmail.com>
2021-10-18 10:42:43 -0600

    guia ejemplo copiada

--- Guia 11-Firebase/birthday/App.js
+++ Guia 11-Firebase/birthday/App.js
@@ -1,112 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, SafeAreaView, StatusBar, LogBox} from 'react-native';
+import base64 from 'react-native-base64';
+import Auth from './src/components/Auth';
+import firebase from './src/utils/firebase';
+import 'firebase/auth';
+import ListBirthday from './src/components/ListBirthday';
+function btoa(data) {
+  return new base64(data, 'binary').toString('base64');
+}
+function atob(data) {
+  return new base64(data, 'base64').toString('binary');
+}
+LogBox.ignoreAllLogs(['Setting a timer']);
+export default function App() {
+  const [user, setUser] = useState(undefined);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(response => {
+      setUser(response);
+    });
+  }, []);
+  if (user === undefined) return null;
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.background}>
+        {user ? <ListBirthday user={user} /> : <Auth />}
+      </SafeAreaView>
+    </>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
+}
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  background: {
+    backgroundColor: '#15212b',
+    height: '100%',
   },
 });
-
-export default App;
---
+++ Guia 11-Firebase/birthday/src/utils/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAOQSrGSO6y3x5leJaphC8rYCSRzJDK8xY',
+  authDomain: 'birthday-f6bf4.firebaseapp.com',
+  projectId: 'birthday-f6bf4',
+  storageBucket: 'birthday-f6bf4.appspot.com',
+  messagingSenderId: '495479740051',
+  appId: '1:495479740051:web:f7761ac02cc53a8113df66',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
